84749c94706e698090cbe034e7fc4ca43dd390e5
Daniel Aviles <daaviles3@espe.edu.ec>
2024-05-17 23:30:10 -0500

    Formulario

--- Aplicativo Web/mail/contact_me.php
+++ Aplicativo Web/mail/contact_me.php
@@ -1,26 +1,34 @@
 <?php
-// Check for empty fields
-if(empty($_POST['name'])      ||
-   empty($_POST['email'])     ||
-   empty($_POST['phone'])     ||
-   empty($_POST['message'])   ||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
-   {
-   echo "No arguments Provided!";
-   return false;
-   }
-   
-$name = strip_tags(htmlspecialchars($_POST['name']));
-$email_address = strip_tags(htmlspecialchars($_POST['email']));
-$phone = strip_tags(htmlspecialchars($_POST['phone']));
-$message = strip_tags(htmlspecialchars($_POST['message']));
-   
-// Create the email and send the message
-$to = 'yourname@yourdomain.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";   
-mail($to,$email_subject,$email_body,$headers);
-return true;         
+function sanitizeInput($data) {
+    return htmlspecialchars(stripslashes(trim($data)));
+}
+
+$name = sanitizeInput($_POST['name']);
+$mail = filter_var(sanitizeInput($_POST['mail']), FILTER_VALIDATE_EMAIL);
+$phone = sanitizeInput($_POST['phone']);
+$message = sanitizeInput($_POST['message']);
+
+if ($mail) {
+    $header = 'From: ' . $mail . " \r\n";
+    $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+    $header .= "Mime-Version: 1.0 \r\n";
+    $header .= "Content-Type: text/plain";
+
+    $fullMessage = "Este mensaje fue enviado por: " . $name . " \r\n";
+    $fullMessage .= "Su e-mail es: " . $mail . " \r\n";
+    $fullMessage .= "Teléfono de contacto: " . $phone . " \r\n";
+    $fullMessage .= "Mensaje: " . $message . " \r\n";
+    $fullMessage .= "Enviado el: " . date('d/m/Y', time());
+
+    $para = 'daaviles3@espe.edu.ec';
+    $asunto = 'Mensaje de... (Escribe como quieres que se vea el remitente de tu correo)';
+
+    if (mail($para, $asunto, utf8_decode($fullMessage), $header)) {
+        header("Location: index.html");
+    } else {
+        echo "Error al enviar el correo.";
+    }
+} else {
+    echo "Email no válido.";
+}
 ?>
